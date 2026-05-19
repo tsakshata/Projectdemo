f85128d744afe692b162914889384a63a63378d5
Akshata T S <tsakshata@gmail.com>
2026-05-19 11:53:25 +0530

    Added the student information

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Project Started");
+const form = document.getElementById("studentForm");
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    successMessage.innerText =
+        "Student Registered Successfully!";
+
+    form.reset();
+
+});
